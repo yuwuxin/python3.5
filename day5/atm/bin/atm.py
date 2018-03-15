d016e54e0b7c8de120c9eaca9f8d771729a910f5
lihang-home <lihang@163.com>
2018-03-15 23:19:11 +0800

    day5 atm

---
+++ day5/atm/bin/atm.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# _*_coding:utf-8_*_
+
+import os
+import sys
+
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(base_dir)
+sys.path.append(base_dir)
+
